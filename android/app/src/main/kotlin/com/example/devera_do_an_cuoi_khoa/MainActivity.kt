502b436a92a5086fdc70039f9c9891588124f72a
NhatTan-uit <19522171@gm.uit.edu.vn>
2022-08-09 09:45:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/devera_do_an_cuoi_khoa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devera_do_an_cuoi_khoa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/widgets/loading_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Loading_Screen extends StatelessWidget {
+  const Loading_Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
